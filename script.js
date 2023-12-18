e63457d0fdf826338ecce2d63640faa87f5158b6
errEstiak <estiak35-2998@diu.edu.bd>
2023-12-19 00:56:42 +0600

    updating menu bar

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-const icons = document.querySelectorAll(".section-1__icon i");
-
-document.querySelector(".menu").addEventListener("click", () => {
-  document.querySelectorAll(".target").forEach((item) => {
+document.querySelector('.menu').addEventListener('click', () => {
+  document.querySelectorAll('.target').forEach((item) => {
     item.classList.toggle('visible');
-  });
-});
+  })
+})
 
-let i = 1;
+const icons = document.querySelectorAll('.section-1__icon i');
+let i = 1
 
 setInterval(() => {
-  i++;
+  i++
+  const icon = document.querySelector('.section-1__icon .visible')
 
-  const icon = document.querySelector(".section-1__icon .visible");
-
-  icon.classList.remove("visible");
+  icon.classList.remove('visible');
 
   if (i > icons.length) {
-    icons[0].classList.add("visible");
-    i = 1;
-  } else {
-    icon.nextElementSibling.classList.add("visible");
-  }
+		icons[0].classList.add('visible')
+		i = 1
+	} else {
+		icon.nextElementSibling.classList.add('visible')
+	}
 }, 3000);
